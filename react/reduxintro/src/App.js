375b54ca999821309717b03eb1d0a1572c2affda
Vidhi-Mungla <134635193+Vidhi-Mungla@users.noreply.github.com>
2023-12-06 20:19:27 +0530

    Update App.js

--- react/reduxintro/src/App.js
+++ react/reduxintro/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { Increment,Decrement } from './action';
+import { Increment, Decrement } from './action';
 
 function App()
- {
+{
   const dispatch = useDispatch();
   const counter = useSelector(state => state.count)
-  return (
 
+  return (
     <div>
 
     <button onClick={()=>{dispatch(Increment())}}>+</button>
       {counter}
     <button onClick={()=>{dispatch(Decrement())}}>-</button>
-    
+
     </div>
   );
 }
